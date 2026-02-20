c522b2196d090ef028b84a3f1b133ff16cd2b436
Vasiliy Nikolskiy <vasilynikolskiy@gmail.com>
2026-02-21 02:57:53 +0300

    Update restaraunt.py
    указал тип возвращаемого значения

--- restaraunts/src/restaraunts/endpoints/v1/restaraunt.py
+++ restaraunts/src/restaraunts/endpoints/v1/restaraunt.py
@@ -1,21 +1,23 @@
 from fastapi import APIRouter
-from ...schemas.restaraunt import Restaraunt
+from restaraunts.schemas.restaraunt import Restaraunt
 from datetime import datetime
-from ...schemas.menu import Menu
+from restaraunts..schemas.menu import Menu
 
 router = APIRouter(tags=['restaraunt'])
 
 @router.get('/restaraunt')
 async def get_restaraunts():
     ...
+
+
 @router.get('/restaraunt/{restaraunt_id}')
 async def get_restaraunt(
         restaraunt_id: str
-):
+)-> Restaraunt :
     return Restaraunt(
         id= restaraunt_id,
         created_at= datetime.now(),
         updated_at= datetime.now(),
         name= 'test',
-        menu= Menu,
+        menu= [],
         orders= [])
